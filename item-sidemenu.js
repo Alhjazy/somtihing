b12e824e45f1d50603b52eea3fc3d97313cbabd3
Ahmad Hjazy <mmmedo73@gmail.com>
2018-12-19 17:32:35 +0300

    Create item-sidemenu.js

---
+++ item-sidemenu.js
@@ -0,0 +1,28 @@
+var data = [{
+    text: 'Product Management',
+    iconCls: 'icon-sum',
+    state: 'open',
+    children: [{
+        text: 'Registration'
+    },{
+        text: 'List'
+    },
+    {
+        text: 'Categories',
+        children: [{
+            text: 'Registration'
+        },{
+            text: 'List'
+        }]
+    }]
+},{
+    text: 'SETTINGS',
+    iconCls: 'icon-more',
+    children: [{
+        text: 'Users'
+    },{
+        text: 'Option5'
+    },{
+        text: 'Option6'
+    }]
+}];
